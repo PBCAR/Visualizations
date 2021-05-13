1defb09c00fe5c1cf0d53d317df0d01418829910
Kyla Belisario <71769015+kbelisar@users.noreply.github.com>
2021-05-13 16:21:05 -0400

    Add files via upload

--- Alluvials.R
+++ Alluvials.R
@@ -6,7 +6,7 @@
 #################################################################################################
 ### a) CHANGE file directory - GO TO: SESSION > SET WORKING DIRECTORY > CHOOSE DIRECTORY
 
-setwd("~/Desktop/PBCAR")   ### CHANGE FILE DIRECTORY
+#setwd("~/Desktop/PBCAR")   ### CHANGE FILE DIRECTORY
 
 ### b) CHANGE THE NAME OF THE FILE (MUST BE A .csv file)
 
@@ -38,7 +38,6 @@ DATA.VIZ <- read.csv(df.name)
 DATA.VIZ <- DATA.VIZ %>% rename(ID = all_of(id.var),
                                 TIME = all_of(time.var))
 
-
 ####### CHANGE: TRANSFORM THE CATEGORICAL VARIABLE
 # -------------- i) Name of the categorical variable after `DATA.VIZ$`
 # -------------- ii) Level values (reverse categories - the levels will fill from the top down)
@@ -47,7 +46,6 @@ DATA.VIZ <- DATA.VIZ %>% rename(ID = all_of(id.var),
 DATA.VIZ$cat.var <- factor(DATA.VIZ$variable.name,           # i) CHANGE VARIABLE NAME HERE
                            levels = c(3,2,1),                # ii) CHANGE LEVELS
                            labels = c("three","two","one"))  # iii) CHANGE LABELS
-
 
 if(type_count == "numeric"){
   DVIZ <- DATA.VIZ %>% group_by(ID,TIME) %>% add_count(cat.var)
@@ -63,39 +61,42 @@ if(type_count == "numeric"){
 plot <- ggplot(DVIZ, aes(x = TIME, stratum = cat.var, alluvium = ID,y = percent,
                          fill = cat.var, label = cat.var)) +
 scale_x_discrete(expand = c(.1, .1)) +
-geom_flow(alpha = 0.5) +
-geom_stratum(alpha = 0.5) +
+geom_flow(alpha = 0.5, show.legend = F) +
+geom_stratum(alpha = 0.5, show.legend = F) +
   geom_text(stat = "stratum") +
-  theme_classic()
+  theme_classic() +
+  theme(axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold"),
+        title = element_text(face = "bold", size = 15))
 } else if(type_count == "percent"){
 plot <- ggplot(DVIZ, aes(x = TIME, stratum = cat.var, alluvium = ID, y = percent,
                         fill = cat.var, label = cat.var)) +
   scale_x_discrete(expand = c(.1, .1)) +
-  geom_flow(alpha = 0.5) +
-  geom_stratum(alpha = 0.5) +
+  geom_flow(alpha = 0.5, show.legend = F) +
+  geom_stratum(alpha = 0.5, show.legend = F) +
   geom_text(stat = "stratum") +
-  theme_classic()
+  theme_classic() +
+  theme(axis.title.x = element_text(face = "bold"),
+        axis.title.y = element_text(face = "bold"),
+        title = element_text(face = "bold", size = 15))
 }
 
 ##### ----------  THE PLOT ABOVE IS THE FOUNDATION OF THE ALLUVIAL
 #################################################################################################
-### BELOW, ARE SOME CHANGES THAT CAN BE MADE TO THIS FOUNDATION
+### BELOW, ARE SOME CHANGES AND MODIFICATIONS THAT CAN BE MADE TO THIS FOUNDATION
 
-plot +
+(plot <- plot +
 
 # ----- CHANGE THE TITLES OF THE X AND Y AXES (`xlab` & `ylab`, respectively)
    xlab("Time") + ylab("") +
-
-# ----- CHANGE THE TITLE OF THE LEGEND
-  labs(fill = "") +
   
 # ----- CHANGE THE TITLE OF THE PLOT
-   labs(title = "Title of Alluvial Plot")
+   labs(title = "Title of Alluvial Plot"))
 
 
-##### ----------  OTHER CHANGES TO MAKE
+##### ----------  OTHER CLOUD PLOT CHANGES TO MAKE
 #################################################################################################
-### CHOOSE COLOUR-BLIND FRIENDLY COLOURS
+### Make the plot colour-blind friendly using the following package:
 
 library(viridis)
 
